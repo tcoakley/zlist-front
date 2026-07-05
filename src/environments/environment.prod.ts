c96d54b312f5270cbbfbff29da8deb3a61844a0a
Tom Coakley <ebuntom@gmail.com>
2026-07-05 16:33:32 -0700

    Turning maintanence mode off. Final direct push of main branch.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
 	production: true,
-	maintenanceMode: true,
+	maintenanceMode: false,
 	apiUrl: 'https://api.zchecklist.com',
 	recaptchaSiteKey: '6Lf2ouMsAAAAACZ6IX2lDqbyxYQ83reVBJNRnxqC',
 	stripePublishableKey: 'pk_live_51TVtkH2OiajZiGUjWGG8Zawf9gKPhhTmgcMAeUMRd3v5539QMylsR4bWwkcQhRIgPMW8sdMCstcCDQi9AeGItbR200cxr8TaBl',
